ad5237285442c12f6ae6dce18c8116ea2a252eab
Victor Cerqueira Barros <viccerqueirabarros@hotmail.com>
2023-07-15 10:39:54 -0300

    Update BancoApplicationTests.java

--- src/test/java/br/com/banco/BancoApplicationTests.java
+++ src/test/java/br/com/banco/BancoApplicationTests.java
@@ -1,13 +1,49 @@
 package br.com.banco;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import br.com.banco.domain.Transferencia;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BancoApplicationTests {
 
+
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
     @Test
-    void contextLoads() {
+    void testBuscarTransacoesPorNomeOperador() {
+        String nomeOperador = "Beltrano";
+        String url = "http://localhost:8080"  + "/transferencia?nomeOperador=" + nomeOperador;
+        ResponseEntity<Transferencia[]> responseEntity = restTemplate.getForEntity(url, Transferencia[].class);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        Transferencia[] transferencias = responseEntity.getBody();
+        assertThat(transferencias).isNotNull();
+        assertThat(transferencias.length).isGreaterThan(0);
     }
 
+    @Test
+    void testBuscarTransacoesPorPeriodo() {
+        // Defina o período de tempo desejado
+        LocalDateTime inicioPeriodo = LocalDateTime.of(2019, 1, 1, 0, 0, 0);
+        LocalDateTime fimPeriodo = LocalDateTime.of(2022, 12, 31, 23, 59, 59);
+
+        String url = "http://localhost:8080" + "/transferencia?inicioPeriodo=" + inicioPeriodo + "&fimPeriodo=" + fimPeriodo;
+        ResponseEntity<Transferencia[]> responseEntity = restTemplate.getForEntity(url, Transferencia[].class);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        Transferencia[] transferencias = responseEntity.getBody();
+        assertThat(transferencias).isNotNull();
+    }
 }
