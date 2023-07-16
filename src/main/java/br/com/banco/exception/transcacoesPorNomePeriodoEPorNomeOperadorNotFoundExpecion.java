ecbfa2bd953aea8361183838ea7b895d46ad56de
Victor Cerqueira Barros <viccerqueirabarros@hotmail.com>
2023-07-16 20:55:56 -0300

    Adicionando tratamento de excpecion

---
+++ src/main/java/br/com/banco/exception/transcacoesPorNomePeriodoEPorNomeOperadorNotFoundExpecion.java
@@ -0,0 +1,8 @@
+package br.com.banco.exception;
+
+public class transcacoesPorNomePeriodoEPorNomeOperadorNotFoundExpecion extends RuntimeException {
+    public transcacoesPorNomePeriodoEPorNomeOperadorNotFoundExpecion ( String mensagem){
+        super(mensagem);
+    }
+    
+}
