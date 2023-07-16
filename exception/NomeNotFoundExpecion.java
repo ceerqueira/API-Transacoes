ecec3550b3aabf03dff509bdbb6cc4b2d397e8a2
Victor Cerqueira Barros <viccerqueirabarros@hotmail.com>
2023-07-16 20:55:27 -0300

    Delete exception directory

--- exception/NomeNotFoundExpecion.java
+++
@@ -1,8 +0,0 @@
-package br.com.banco.exception;
-
-public class NomeNotFoundExpecion extends RuntimeException{
-    public NomeNotFoundExpecion (String mensagem){
-        super(mensagem);
-    }
-    
-}
