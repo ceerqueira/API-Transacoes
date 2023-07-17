a6fd1366e005c8cbc880f9866b15c6fdf94f7b47
Victor Cerqueira Barros <viccerqueirabarros@hotmail.com>
2023-07-17 00:24:56 -0300

    Update NomeNotFoundExpecion.java

--- src/main/java/br/com/banco/exception/NomeNotFoundExpecion.java
+++ src/main/java/br/com/banco/exception/NomeNotFoundExpecion.java
@@ -1,5 +1,9 @@
 package br.com.banco.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class NomeNotFoundExpecion extends RuntimeException{
     public NomeNotFoundExpecion (String mensagem){
         super(mensagem);
