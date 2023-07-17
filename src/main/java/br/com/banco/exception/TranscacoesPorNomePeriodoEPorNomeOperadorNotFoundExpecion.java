e7449bb6ceb30cf7f98e0d0288d0e80328cc03d9
Victor Cerqueira Barros <viccerqueirabarros@hotmail.com>
2023-07-16 21:45:34 -0300

    Correção no padrão de desenvolvimento de nomenclatura

---
+++ src/main/java/br/com/banco/exception/TranscacoesPorNomePeriodoEPorNomeOperadorNotFoundExpecion.java
@@ -0,0 +1,8 @@
+package br.com.banco.exception;
+
+public class TranscacoesPorNomePeriodoEPorNomeOperadorNotFoundExpecion extends RuntimeException {
+    public TranscacoesPorNomePeriodoEPorNomeOperadorNotFoundExpecion ( String mensagem){
+        super(mensagem);
+    }
+    
+}
--- src/main/java/br/com/banco/exception/transcacoesPorNomePeriodoEPorNomeOperadorNotFoundExpecion.java
+++
@@ -1,8 +0,0 @@
-package br.com.banco.exception;
-
-public class transcacoesPorNomePeriodoEPorNomeOperadorNotFoundExpecion extends RuntimeException {
-    public transcacoesPorNomePeriodoEPorNomeOperadorNotFoundExpecion ( String mensagem){
-        super(mensagem);
-    }
-    
-}
