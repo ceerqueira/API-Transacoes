578150edbd25efa40b22bf8c7bfbd3496dbd1c8e
Victor Cerqueira Barros <viccerqueirabarros@hotmail.com>
2023-07-17 00:25:31 -0300

    Update TranscacoesPorNomePeriodoEPorNomeOperadorNotFoundExpecion.java

--- src/main/java/br/com/banco/exception/TranscacoesPorNomePeriodoEPorNomeOperadorNotFoundExpecion.java
+++ src/main/java/br/com/banco/exception/TranscacoesPorNomePeriodoEPorNomeOperadorNotFoundExpecion.java
@@ -1,5 +1,9 @@
 package br.com.banco.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class TranscacoesPorNomePeriodoEPorNomeOperadorNotFoundExpecion extends RuntimeException {
     public TranscacoesPorNomePeriodoEPorNomeOperadorNotFoundExpecion ( String mensagem){
         super(mensagem);
