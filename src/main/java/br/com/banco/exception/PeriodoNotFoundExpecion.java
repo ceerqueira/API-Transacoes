6a2325f07f21f3f569df594ecfc2c7d08751c241
Victor Cerqueira Barros <viccerqueirabarros@hotmail.com>
2023-07-17 00:25:12 -0300

    Update PeriodoNotFoundExpecion.java

--- src/main/java/br/com/banco/exception/PeriodoNotFoundExpecion.java
+++ src/main/java/br/com/banco/exception/PeriodoNotFoundExpecion.java
@@ -1,5 +1,9 @@
 package br.com.banco.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NO_CONTENT)
 public class PeriodoNotFoundExpecion extends RuntimeException{
 
     public PeriodoNotFoundExpecion(String mensagem){
